1747d028d74437d3c5d0a3bdda074bfc25642bfb
Arit Mandal <aritmandal@myyahoo.com>
2025-04-21 11:18:45 +0530

    commit

---
+++ Sum and Product .py
@@ -0,0 +1,8 @@
+def add_product():
+    X=int(input("First Number:"))
+    Y=int(input("Second Number:"))
+    Z=int(input("Third Number:"))
+    B=X+Y+Z
+    print("Sum:",B)
+    C=X*Y*Z
+    print("Product:",C)
